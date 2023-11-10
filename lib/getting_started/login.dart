9891181ae84199d0a57610d9dc41eb59b7ef0691
Felix Cena Jr. <felixcena15@gmail.com>
2023-11-11 00:28:19 +0800

    first try

--- lib/getting_started/login.dart
+++ lib/getting_started/login.dart
@@ -5,6 +5,10 @@ class Login extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        child: const Text('Gwapo'),
+      ),
+    );
   }
 }
