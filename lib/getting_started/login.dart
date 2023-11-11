501a117960796b68b60f1cc05e0afa9f15cac39f
Felix Cena Jr. <felixcena15@gmail.com>
2023-11-11 22:33:36 +0800

    finishing the login screen

--- lib/getting_started/login.dart
+++ lib/getting_started/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -33,16 +34,17 @@ class LoginScreen extends StatelessWidget {
                   ],
                 ),
                 const SizedBox(
-                  height: 20,
+                  height: 34,
                 ),
                 Form(
                   child: Column(
                     children: [
                       TextFormField(
-                        decoration: const InputDecoration(
+                        decoration: InputDecoration(
                             border: OutlineInputBorder(
-                                borderSide: BorderSide(width: 2)),
-                            prefixIcon: Icon(
+                                borderRadius: BorderRadius.circular(14),
+                                borderSide: const BorderSide(width: 2)),
+                            prefixIcon: const Icon(
                               Icons.person,
                             ),
                             labelText: 'Username'),
@@ -51,18 +53,21 @@ class LoginScreen extends StatelessWidget {
                         height: 16,
                       ),
                       TextFormField(
-                        decoration: const InputDecoration(
+                        decoration: InputDecoration(
                             border: OutlineInputBorder(
-                                borderSide: BorderSide(width: 2)),
-                            prefixIcon: Icon(
+                                borderRadius: BorderRadius.circular(14),
+                                borderSide: const BorderSide(width: 2)),
+                            prefixIcon: const Icon(
                               Icons.lock,
                             ),
                             labelText: 'Password',
-                            suffixIcon: Icon(Icons.remove_red_eye_outlined)),
+                            suffixIcon:
+                                const Icon(Icons.remove_red_eye_outlined)),
                       ),
                     ],
                   ),
                 ),
+
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
@@ -79,27 +84,124 @@ class LoginScreen extends StatelessWidget {
                       ],
                     ),
                     TextButton(
-                        onPressed: () {},
-                        child: Text(
-                          'Forgot Password',
-                          style: Theme.of(context).textTheme.headlineSmall,
-                        )),
+                      onPressed: () {},
+                      child: Text(
+                        'Forgot Password',
+                        style: Theme.of(context).textTheme.headlineSmall,
+                      ),
+                    ),
                   ],
                 ),
 
-                const SizedBox(height: 20),
+                const SizedBox(height: 34),
                 SizedBox(
                     width: double.infinity,
                     child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          elevation: 0,
+                          foregroundColor: Colors.white,
+                          backgroundColor: Colors.purple[400],
+                          padding: const EdgeInsets.symmetric(vertical: 18),
+                          textStyle: GoogleFonts.poppins(
+                              textStyle: const TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w600,
+                                  color: Colors.white)),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12))),
                       onPressed: () {},
                       child: const Text('Sign In'),
                     )),
+                const SizedBox(
+                  height: 15,
+                ),
                 SizedBox(
-                    width: double.infinity,
-                    child: OutlinedButton(
-                      onPressed: () {},
-                      child: const Text('Create Account'),
-                    ))
+                  width: double.infinity,
+                  child: OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                        elevation: 0,
+                        foregroundColor: Colors.black,
+                        side: const BorderSide(color: Colors.purple),
+                        textStyle: GoogleFonts.poppins(
+                          textStyle: const TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.white),
+                        ),
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 20, vertical: 16),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(14))),
+                    onPressed: () {},
+                    child: const Text('Create Account'),
+                  ),
+                ),
+                const SizedBox(
+                  height: 32,
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Flexible(
+                      child: Divider(
+                        color: Colors.grey,
+                        thickness: .5,
+                        indent: 60,
+                        endIndent: 5,
+                      ),
+                    ),
+                    Text(
+                      'Or Sign in With',
+                      style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.normal,
+                          color: Colors.grey),
+                    ),
+                    Flexible(
+                      child: Divider(
+                        color: Colors.grey,
+                        thickness: .5,
+                        indent: 5,
+                        endIndent: 60,
+                      ),
+                    )
+                  ],
+                ),
+                const SizedBox(
+                  height: 14,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey),
+                        borderRadius: BorderRadius.circular(100),
+                      ),
+                      child: IconButton(
+                          onPressed: () {},
+                          icon: const Image(
+                              width: 30,
+                              height: 30,
+                              image: AssetImage('assets/icons/facebook.png'))),
+                    ),
+                    const SizedBox(
+                      width: 16,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey),
+                        borderRadius: BorderRadius.circular(100),
+                      ),
+                      child: IconButton(
+                          onPressed: () {},
+                          icon: const Image(
+                              width: 30,
+                              height: 30,
+                              image: AssetImage('assets/icons/google.png'))),
+                    )
+                  ],
+                )
               ],
             ),
           ),
