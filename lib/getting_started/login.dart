eac4b9f0fc08dd5051037dc6c8f134cd2de2d920
Felix Cena Jr. <felixcena15@gmail.com>
2023-11-11 17:25:09 +0800

    initial login screen

--- lib/getting_started/login.dart
+++ lib/getting_started/login.dart
@@ -1,18 +1,107 @@
 import 'package:flutter/material.dart';
 
-class Login extends StatelessWidget {
-  const Login({super.key});
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: SizedBox(
-          height: 50,
-          width: 50,
-          child: Text(
-            'Gwapo',
-            style: Theme.of(context).textTheme.headlineSmall,
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              children: [
+                //header
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Image(
+                        height: 100, image: AssetImage('assets/img/logo.png')),
+                    Text(
+                      'Welcome,',
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                    const SizedBox(
+                      height: 8.0,
+                    ),
+                    Text(
+                      'I love you, thank you for using our app once again, Login now',
+                      textAlign: TextAlign.justify,
+                      style: Theme.of(context).textTheme.headlineSmall,
+                    )
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Form(
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        decoration: const InputDecoration(
+                            border: OutlineInputBorder(
+                                borderSide: BorderSide(width: 2)),
+                            prefixIcon: Icon(
+                              Icons.person,
+                            ),
+                            labelText: 'Username'),
+                      ),
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                            border: OutlineInputBorder(
+                                borderSide: BorderSide(width: 2)),
+                            prefixIcon: Icon(
+                              Icons.lock,
+                            ),
+                            labelText: 'Password',
+                            suffixIcon: Icon(Icons.remove_red_eye_outlined)),
+                      ),
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Checkbox(
+                          value: true,
+                          onChanged: (value) {},
+                        ),
+                        Text(
+                          'Remember Me',
+                          style: Theme.of(context).textTheme.headlineSmall,
+                        )
+                      ],
+                    ),
+                    TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          'Forgot Password',
+                          style: Theme.of(context).textTheme.headlineSmall,
+                        )),
+                  ],
+                ),
+
+                const SizedBox(height: 20),
+                SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: const Text('Sign In'),
+                    )),
+                SizedBox(
+                    width: double.infinity,
+                    child: OutlinedButton(
+                      onPressed: () {},
+                      child: const Text('Create Account'),
+                    ))
+              ],
+            ),
           ),
         ),
       ),
