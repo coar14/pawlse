8cb264f3b39343e8ae4f19b3934c8be36283ab85
Felix Cena Jr. <felixcena15@gmail.com>
2023-11-17 00:41:01 +0800

    event section

--- lib/getting_started/auth_page.dart
+++ lib/getting_started/auth_page.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:pawlse/HOMEPAGE/event_section.dart';
 import 'package:pawlse/HOMEPAGE/home_page.dart';
 import 'package:pawlse/getting_started/login.dart';
 
@@ -13,7 +14,7 @@ class AuthPage extends StatelessWidget {
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return const HomePage();
+            return const EventSectionUser();
           } else {
             return const LoginScreen();
           }
